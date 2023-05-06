0ab93b82accad8b5d6feb8a2c52a430b981ab7ee
Sebah40 <106514501+Sebah40@users.noreply.github.com>
2023-05-05 21:22:06 -0300

    Removed print check method commence fail

--- src/main/java/com/portfolio/Sebastian/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/Sebastian/Security/Controller/AuthController.java
@@ -80,7 +80,6 @@ public class AuthController {
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
-        System.out.println("ASDASDASD");
 
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
         loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
