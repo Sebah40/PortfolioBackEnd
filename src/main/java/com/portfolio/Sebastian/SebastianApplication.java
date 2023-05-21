b990b8b65d11cfad84c470d13d188ccd245407ad
Sebah40 <106514501+Sebah40@users.noreply.github.com>
2023-05-20 21:29:54 -0300

    Backend fix for Projects and education 2

--- src/main/java/com/portfolio/Sebastian/SebastianApplication.java
+++ src/main/java/com/portfolio/Sebastian/SebastianApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SebastianApplication {
 
 	public static void main(String[] args) {
+            System.out.println("UPDATE");
 		SpringApplication.run(SebastianApplication.class, args);
 	}
 
